dc545928b66124e0436d570e94f18b3ed377502a
Shihong.Qiang <503579266@qq.com>
2016-03-03 17:03:54 +0800

    Network packet first upload

---
+++ network_v1.0/Conf/index.php
@@ -0,0 +1,8 @@
+﻿<html>
+<body>
+<div class="osyunwei_index">
+<p align="center"><font color="#FF0000" size="5">Web服务器环境LNMP一键安装包配置成功！</font></p>
+<p align="center"><font color="#FF0000" size="4">LNMP=Linux+Nginx+MySQL+PHP</font></p>
+</div>
+</body>
+</html>
